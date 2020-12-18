b8c15b166aa64f8148ac1d998d6e0406c8064c10
Sadisha Nimsara <nsadisha@gmail.com>
2020-12-19 01:32:03 +0530

    homework 03

---
+++ oop_hw03_SE-2018-017/src/gui/Login.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel loginPane;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- oop_hw03_SE-2018-017/src/main/Main.java
+++ oop_hw03_SE-2018-017/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import gui.Login;
+
 public class Main {
     public static void main(String[] args) {
 
