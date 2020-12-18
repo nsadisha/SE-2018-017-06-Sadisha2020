dafaeadc669117cf952b1293bc93eb035c5f9dab
Sadisha Nimsara <nsadisha@gmail.com>
2020-12-19 01:28:23 +0530

    third one done

---
+++ Individual Assignment/src/gui/SimpleButtons.java
@@ -0,0 +1,61 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleButtons extends JFrame{
+    private JPanel panel;
+    private JButton one;
+    private JButton two;
+    private JButton three;
+    private JButton four;
+    private JLabel label;
+
+    public SimpleButtons(){
+        this.setContentPane(panel);
+        this.setTitle("SimpleButtons");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = screenSize.width/2 - 150;
+        int y = screenSize.height/2 - 175;
+        this.setLocation(x, y);
+
+        //handling events
+        this.one.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setLabel("1");
+            }
+        });
+        this.two.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setLabel("2");
+            }
+        });
+        this.three.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setLabel("3");
+            }
+        });
+        this.four.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setLabel("4");
+            }
+        });
+    }
+
+    private void setLabel(String s){
+        this.label.setText(s);
+    }
+
+    public static void main(String[] args) {
+        SimpleButtons gui = new SimpleButtons();
+        gui.setSize(300, 350);
+        gui.setVisible(true);
+    }
+}
